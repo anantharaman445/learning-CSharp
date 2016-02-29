068119f02aae6f4b51094a671b4da219753906b9
Bharath Srinivasan <bharathsoftdev@gmail.com>
2016-02-29 14:19:13 -0800

    Initial files added

---
+++ helloWorld.cs
@@ -0,0 +1,10 @@
+using System;							// default namespace
+
+namespace helloWorldApplication {
+	class Program {
+		static void Main (String[] args) {
+			Console.WriteLine("Hello World App");		// prints string in a new line
+			Console.ReadKey();							// waits for a key press and exit
+		}
+	}
+}
---
+++ whileLoop.cs
@@ -0,0 +1,14 @@
+using System;
+
+class loop
+{
+    static void Main(String[] args)
+    {
+        int a = 0, 1, 2, 3, 4 ;
+        foreach(string s in a)
+        {
+            Console.WriteLine(s);
+        }
+        Console.ReadLine();
+    }
+}
