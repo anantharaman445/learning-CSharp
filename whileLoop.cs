6addf332248d1171a1d6d76dee1e0f6ce6b54fc4
Bharath Srinivasan <mailbharathyes@gmail.com>
2016-03-07 23:39:06 +0530

    Corrected array initialization

--- whileLoop.cs
+++ whileLoop.cs
@@ -4,7 +4,7 @@ class loop
 {
     static void Main(String[] args)
     {
-        int a = 0, 1, 2, 3, 4 ;
+        int[] a = new int[] { 0, 1, 2, 3, 4 } ;
         foreach(string s in a)
         {
             Console.WriteLine(s);
